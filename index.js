ebcd82fc87fab2baf009407aedcef9517b54e16d
zhuozhongzhi <zhongzhi_z@gzleihou.cn>
2018-01-31 12:25:48 +0800

    version 1.1 update

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-import './style.css'
 /**
  *@description 观察者模式 全局监听富文本编辑器
  */
 export const QuillWatch = {
     watcher: {},  // 登记编辑器信息
     active: null,  // 当前触发的编辑器
-    on: function(imageExtendId, ImageExtend) {  // 登记注册使用了ImageEXtend的编辑器
+    on: function (imageExtendId, ImageExtend) {  // 登记注册使用了ImageEXtend的编辑器
         if (!this.watcher[imageExtendId]) {
             this.watcher[imageExtendId] = ImageExtend
         }
     },
-    emit: function(activeId) {  // 事件发射触发
-        imgHandler()
+    emit: function (activeId) {  // 事件发射触发
         this.active = this.watcher[activeId]
+        imgHandler()
     }
 }
 
@@ -32,17 +31,6 @@ export class ImageExtend {
         this.config = config
         this.file = ''  // 要上传的图片
         this.imgURL = ''  // 图片地址
-        let quillLoading
-        if (this.config.loading) {
-            quillLoading = document.querySelector('.quillLoading')
-            if (quillLoading === null) {
-                quillLoading = document.createElement('div')
-                quillLoading.classList.add('quill-loading')
-                quillLoading.classList.add('extend-upload-success')
-                document.querySelector('.ql-container').appendChild(quillLoading)
-            }
-        }
-        this.quillLoading = quillLoading
         quill.root.addEventListener('paste', this.pasteHandle.bind(this), false)
         quill.root.addEventListener('drop', this.dropHandle.bind(this), false)
         quill.root.addEventListener('dropover', function (e) {
@@ -80,14 +68,10 @@ export class ImageExtend {
                 this.file = item.getAsFile()
                 let self = this
                 // 如果图片限制大小
-                if (self.config.size && self.file.size >= self.config.size * 1024 * 1024 && self.config.loading) {
-                    self.quillLoading.classList.remove('extend-upload-success')
-                    self.quillLoading.classList.add('extend-upload-warning-color')
-                    self.quillLoading.innerHTML = '图片大小超过限制'
-                    setTimeout(function () {
-                        self.quillLoading.classList.remove('extend-upload-warning-color')
-                        self.quillLoading.classList.add('extend-upload-success')
-                    }, 1500)
+                if (self.config.size && self.file.size >= self.config.size * 1024 * 1024) {
+                    if (self.config.sizeError) {
+                        self.config.sizeError()
+                    }
                     return
                 }
                 if (this.config.action) {
@@ -107,14 +91,10 @@ export class ImageExtend {
         const self = this
         e.preventDefault()
         // 如果图片限制大小
-        if (self.config.size && self.file.size >= self.config.size * 1024 * 1024 && self.config.loading) {
-            self.quillLoading.classList.add('extend-upload-warning-color')
-            self.quillLoading.classList.remove('extend-upload-success')
-            self.quillLoading.innerHTML = '图片大小超过限制'
-            setTimeout(function () {
-                self.quillLoading.classList.remove('extend-upload-warning-color')
-                self.quillLoading.classList.add('extend-upload-success')
-            }, 1500)
+        if (self.config.size && self.file.size >= self.config.size * 1024 * 1024) {
+            if (self.config.sizeError) {
+                self.config.sizeError()
+            }
             return
         }
         self.file = e.dataTransfer.files[0]; // 获取到第一个上传的文件对象
@@ -166,32 +146,30 @@ export class ImageExtend {
         xhr.onload = function (e) {
             if (self.config.loading) {
                 setTimeout(function () {
-                    quillLoading.classList.add('extend-upload-success')
                 }, 1000)
             }
             if (xhr.status === 200) {
-                self.quill.root.innerHTML = self.quill.root.innerHTML.replace('[uploading...]', '')
+                // self.quill.root.innerHTML = self.quill.root.innerHTML.replace('[uploading...]', '')
                 let res = JSON.parse(xhr.responseText)
                 self.imgURL = config.response(res)
+                QuillWatch.active.uploadSuccess()
                 self.insertImg()
                 if (self.config.success) {
                     self.config.success()
                 }
             } else {
-                self.quill.root.innerHTML = self.quill.root.innerHTML.replace('[uploading...]', '[upload error]')
+                // self.quill.root.innerHTML = self.quill.root.innerHTML.replace('[uploading...]', '[upload error]')
                 if (self.config.error) {
                     self.config.error()
                 }
+                QuillWatch.active.uploadError()
             }
         }
         // 开始上传数据
         xhr.upload.onloadstart = function (e) {
-
-            let length = (self.quill.getSelection() || {}).index || self.quill.getLength()
-            self.quill.insertText(length, '[uploading...]', { 'color': 'red'}, true)
-            if (self.config.loading) {
-                quillLoading.classList.remove('extend-upload-success')
-            }
+            QuillWatch.active.uploading()
+            // let length = (self.quill.getSelection() || {}).index || self.quill.getLength()
+            // self.quill.insertText(length, '[uploading...]', { 'color': 'red'}, true)
             if (config.start) {
                 config.start()
             }
@@ -199,21 +177,11 @@ export class ImageExtend {
         // 上传过程
         xhr.upload.onprogress = function (e) {
             let complete = (e.loaded / e.total * 100 | 0) + '%'
-            if (self.config.loading) {
-                quillLoading.innerHTML = '已上传' + complete
-            }
+            QuillWatch.active.progress(complete)
         }
         // 当发生网络异常的时候会触发，如果上传数据的过程还未结束
         xhr.upload.onerror = function (e) {
-            self.quill.root.innerHTML = self.quill.root.innerHTML.replace('[uploading...]', '[upload error]')
-            if (self.config.loading) {
-                self.quillLoading.classList.add('extend-upload-warning-color')
-                quillLoading.innerHTML = '网络异常，失败请重试'
-                setTimeout(function () {
-                    self.quillLoading.classList.remove('extend-upload-warning-color')
-                    quillLoading.classList.add('extend-upload-success')
-                }, 1500)
-            }
+            QuillWatch.active.uploadError()
             if (config.error) {
                 config.error()
             }
@@ -232,12 +200,40 @@ export class ImageExtend {
      */
     insertImg() {
         const self = this
-        self.quill.blur()
-        let length = (this.quill.getSelection() || {}).index || this.quill.getLength()
-        self.quill.insertEmbed(length, 'image', self.imgURL, 'user')
+        let length = (this.quill.getSelection(true) || {}).index || this.quill.getLength()
+        self.quill.insertEmbed(length, 'image', self.imgURL)
         self.quill.setSelection(length + 1)
     }
 
+    /**
+     * @description 显示上传的进度
+     */
+    progress(pro) {
+        pro = '[' + 'uploading' + pro + ']'
+        QuillWatch.active.quill.root.innerHTML
+            = QuillWatch.active.quill.root.innerHTML.replace(/\[uploading.*?\]/, pro)
+    }
+
+    /**
+     * 开始上传
+     */
+    uploading() {
+        let length = (QuillWatch.active.quill.getSelection() || {}).index || QuillWatch.active.quill.getLength()
+        QuillWatch.active.quill.insertText(length, '[uploading...]', {'color': 'red'}, true)
+    }
+
+    /**
+     * 上传失败
+     */
+    uploadError() {
+        QuillWatch.active.quill.root.innerHTML
+            = QuillWatch.active.quill.root.innerHTML.replace(/\[uploading.*?\]/, '[upload error]')
+    }
+
+    uploadSuccess() {
+        QuillWatch.active.quill.root.innerHTML
+            = QuillWatch.active.quill.root.innerHTML.replace(/\[uploading.*?\]/, '')
+    }
 }
 
 /**
@@ -256,18 +252,14 @@ export function imgHandler() {
             self.file = fileInput.files[0]
             fileInput.value = ''
             // 如果图片限制大小
-            if (self.config.size && self.file.size >= self.config.size * 1024 * 1024 && self.config.loading) {
-                self.quillLoading.classList.remove('extend-upload-success')
-                self.quillLoading.classList.add('extend-upload-warning-color')
-                self.quillLoading.innerHTML = '图片大小超过限制'
-                setTimeout(function () {
-                    self.quillLoading.classList.remove('extend-upload-warning-color')
-                    self.quillLoading.classList.add('extend-upload-success')
-                }, 1500)
+            if (self.config.size && self.file.size >= self.config.size * 1024 * 1024) {
+                if (self.config.sizeError) {
+                    self.config.sizeError()
+                }
                 return
             }
             if (self.config.action) {
-                self.uploadImg(self.config.change)
+                self.uploadImg()
             } else {
                 self.toBase64()
             }
